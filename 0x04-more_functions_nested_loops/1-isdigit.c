bfbcb2e87c2f2ea03edb520b853c2f074cbcf87f
Assasinator-ops <68000010+Assasinator-ops@users.noreply.github.com>
2021-09-15 19:02:39 +0300

    updated

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,3 +9,13 @@ int _isdigit(int c)
 {
 	return (c >= 48 && c < 58);
 }
+int main(void)
+{
+    char c;
+
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
+}
