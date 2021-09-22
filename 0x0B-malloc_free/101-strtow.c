2080bfc70aa706f93fe8f3d945b63d5ca75b52e9
Assasinator-ops <68000010+Assasinator-ops@users.noreply.github.com>
2021-09-22 16:48:17 +0300

    Updated

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,78 +8,38 @@
 
 char **strtow(char *str)
 {
-	int i = 0, j = 0, k = 0;
-	int len = 0, count = 0;
-	char **f, *col;
+	char **ptr;
+	int i, k, len, start, end, j = 0;
+	int words =  countWords(str);
 
-	if (!str || !*str)
-	{
+	if (!str || !countWords(str))
 		return (NULL);
-	}
-
-	while (*(str + i))
+	ptr = malloc(sizeof(char *) * (words + 1));
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < words; i++)
 	{
-		if (*(str + i) != ' ')
+		start = startIndex(str, j);
+		end = endIndex(str, start);
+		len = end - start;
+		ptr[i] = malloc(sizeof(char) * (len + 1));
+		if (!ptr[i])
 		{
-			if (*(str + i + 1) == ' ' || *(str + i + 1) == 0)
+			i -= 1;
+			while (i >= 0)
 			{
-				count += 1;
+				free(ptr[i]);
+					i--;
 			}
-		}
-		i++;
-	}
-
-	if (count == 0)
-	{
-		return (NULL);
-	}
-	count += 1;
-	f = malloc(sizeof(char *) * count);
-
-	if (!f)
-	{
-		return (NULL);
-	}
-	i = 0;
-
-	while (*str)
-	{
-		while (*str == ' ' && *str)
-		{
-			str++;
-		}
-		len = 0;
-
-		while (*(str + len) != ' ' && *(str + len))
-		{
-			len += 1;
-		}
-		len += 1;
-		col = malloc(sizeof(char) * len);
-
-		if (!col)
-		{
-			for (k = j - 1; k >= 0; k--)
-			{
-				free(f[k]);
-			}
-			free(f);
+			free(ptr);
 			return (NULL);
 		}
-
-		for (k = 0; k < (len - 1);  k++)
-		{
-			*(col + k) = *(str++);
-		}
-		*(col + k) = '\0';
-		*(f + j) = col;
-
-		if (j < (count - 1))
-		{
-			j++;
-		}
+		for (k = 0; k < len; k++)
+			ptr[i][k] = str[start++];
+		ptr[i][k++] = '\0';
+		j = end + 1;
 	}
-	*(f + j) = NULL;
-	return (f);
-} /*yes*/
+	ptr[i] = NULL;
+	return (ptr);
+}
 
