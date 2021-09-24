f2c98079acfa953ddb200122c497b03a8fb289e5
Assasinator-ops <68000010+Assasinator-ops@users.noreply.github.com>
2021-09-24 10:02:27 +0300

    Edited

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,16 @@
 #include "holberton.h"
-#include <stdlib.h>
 
 /**
- *malloc_checked - returns a pointer to the allocated memory
- *@b: integer
- *
- *Return: pointer
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to be malloced
+ * Return: returns a void pointer
  */
-
 void *malloc_checked(unsigned int b)
 {
-char *c;
+	void *ptr;
 
-c = malloc(b);
-
-if (c == NULL)
-exit(98);
-
-return (c);
+	ptr = malloc(b);
+	if (!ptr)
+		exit(98);
+	return (ptr);
 }
